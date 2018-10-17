4e4c0dbd6f37430349fe27f0e09aa0d302aa9aac
ml4963 <ml4963@nyu.edu>
2018-10-16 21:06:57 -0400

    added form

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function createWindow () {
   });
 
   mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
+    pathname: path.join(__dirname, 'login.html'),
     protocol: 'file:',
     slashes: true
   }));
