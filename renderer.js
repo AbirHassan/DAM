d86a5a93052f61b2e7b9946e2531dcddf955c2a2
Abir Hassan <ash521@nyu.edu>
2018-10-16 23:30:36 -0400

    Started adding code for database support

--- renderer.js
+++ renderer.js
@@ -6,11 +6,18 @@ var ById = function (id) {
     const remote = require('electron').remote;
 
     var closeBtn = ById('close-btn');
+    var submitBtn = ById('submit');
     console.log(closeBtn);
+    console.log(submitBtn);
 
     function closeDAM() {
         const window = remote.getCurrentWindow();
         window.close();
+    }
+
+    function submit() {
+        var field = document.getElementById();
+        
     }
 
     closeBtn.addEventListener("click", closeDAM);
